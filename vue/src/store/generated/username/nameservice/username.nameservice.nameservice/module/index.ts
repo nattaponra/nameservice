2c1e907112ecf7a37992af269a91725636ef8848
Nattapon Rakthong <nattapon.rakthong@gmail.com>
2022-06-05 17:36:42 +0700

    Add the MsgBuyName Message

--- vue/src/store/generated/username/nameservice/username.nameservice.nameservice/module/index.ts
+++ vue/src/store/generated/username/nameservice/username.nameservice.nameservice/module/index.ts
@@ -4,9 +4,11 @@ import { StdFee } from "@cosmjs/launchpad";
 import { SigningStargateClient } from "@cosmjs/stargate";
 import { Registry, OfflineSigner, EncodeObject, DirectSecp256k1HdWallet } from "@cosmjs/proto-signing";
 import { Api } from "./rest";
+import { MsgBuyName } from "./types/nameservice/tx";
 
 
 const types = [
+  ["/username.nameservice.nameservice.MsgBuyName", MsgBuyName],
   
 ];
 export const MissingWalletError = new Error("wallet is required");
@@ -39,6 +41,7 @@ const txClient = async (wallet: OfflineSigner, { addr: addr }: TxClientOptions =
 
   return {
     signAndBroadcast: (msgs: EncodeObject[], { fee, memo }: SignAndBroadcastOptions = {fee: defaultFee, memo: ""}) => client.signAndBroadcast(address, msgs, fee,memo),
+    msgBuyName: (data: MsgBuyName): EncodeObject => ({ typeUrl: "/username.nameservice.nameservice.MsgBuyName", value: MsgBuyName.fromPartial( data ) }),
     
   };
 };
