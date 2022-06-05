ad7b99ecd1f8026eadbbd0353aa42b3cf2571e33
Nattapon Rakthong <nattapon.rakthong@gmail.com>
2022-06-05 18:03:31 +0700

    Update vuejs

--- vue/src/store/generated/username/nameservice/username.nameservice.nameservice/index.ts
+++ vue/src/store/generated/username/nameservice/username.nameservice.nameservice/index.ts
@@ -199,21 +199,6 @@ export default {
 		},
 		
 		
-		async sendMsgBuyName({ rootGetters }, { value, fee = [], memo = '' }) {
-			try {
-				const txClient=await initTxClient(rootGetters)
-				const msg = await txClient.msgBuyName(value)
-				const result = await txClient.signAndBroadcast([msg], {fee: { amount: fee, 
-	gas: "200000" }, memo})
-				return result
-			} catch (e) {
-				if (e == MissingWalletError) {
-					throw new Error('TxClient:MsgBuyName:Init Could not initialize signing client. Wallet is required.')
-				}else{
-					throw new Error('TxClient:MsgBuyName:Send Could not broadcast Tx: '+ e.message)
-				}
-			}
-		},
 		async sendMsgSetName({ rootGetters }, { value, fee = [], memo = '' }) {
 			try {
 				const txClient=await initTxClient(rootGetters)
@@ -244,20 +229,22 @@ export default {
 				}
 			}
 		},
-		
-		async MsgBuyName({ rootGetters }, { value }) {
+		async sendMsgBuyName({ rootGetters }, { value, fee = [], memo = '' }) {
 			try {
 				const txClient=await initTxClient(rootGetters)
 				const msg = await txClient.msgBuyName(value)
-				return msg
+				const result = await txClient.signAndBroadcast([msg], {fee: { amount: fee, 
+	gas: "200000" }, memo})
+				return result
 			} catch (e) {
 				if (e == MissingWalletError) {
 					throw new Error('TxClient:MsgBuyName:Init Could not initialize signing client. Wallet is required.')
-				} else{
-					throw new Error('TxClient:MsgBuyName:Create Could not create message: ' + e.message)
+				}else{
+					throw new Error('TxClient:MsgBuyName:Send Could not broadcast Tx: '+ e.message)
 				}
 			}
 		},
+		
 		async MsgSetName({ rootGetters }, { value }) {
 			try {
 				const txClient=await initTxClient(rootGetters)
@@ -281,6 +268,19 @@ export default {
 					throw new Error('TxClient:MsgDeleteName:Init Could not initialize signing client. Wallet is required.')
 				} else{
 					throw new Error('TxClient:MsgDeleteName:Create Could not create message: ' + e.message)
+				}
+			}
+		},
+		async MsgBuyName({ rootGetters }, { value }) {
+			try {
+				const txClient=await initTxClient(rootGetters)
+				const msg = await txClient.msgBuyName(value)
+				return msg
+			} catch (e) {
+				if (e == MissingWalletError) {
+					throw new Error('TxClient:MsgBuyName:Init Could not initialize signing client. Wallet is required.')
+				} else{
+					throw new Error('TxClient:MsgBuyName:Create Could not create message: ' + e.message)
 				}
 			}
 		},
